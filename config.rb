006c55f6977e2962a42fc4b59eea36d94cc05a2c
Edouard <edou4rd@gmail.com>
2014-10-04 22:39:47 +0200

    gem

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Require any additional compass plugins here.
+require 'bootstrap-sass'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "app_folder/static/css"
+sass_dir = "app_folder/static/sass"
+images_dir = "app_folder/static/images"
+
+output_style = :compact
